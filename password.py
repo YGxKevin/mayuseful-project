8d20488a71d8cb9dc69f62ac6b7fd236f51d1221
YGxKevin <47622079+YGxKevin@users.noreply.github.com>
2019-06-09 19:25:39 +0800

    version 1.5

--- password.py
+++ password.py
@@ -6,10 +6,13 @@ while x >= 1 and x <= 3:									# 進入WHILE LOOP
 	if PSW == password:										#注意: 要跟正確密碼不同的宣告 否則將蓋過正確密碼 形成BUG
 		break
 	else:
-		x = x - 1
-		print('Negative you still got', x, 'times chances')
+		x -= 1
+		print('Access Denied')
+		if x == 0:
+			break
+		print('you still got', x, 'times chances')
 
 if PSW == password:
 	print('Access Granted')
 else:
-	print('Access Denied')
+	print('You have no Authorization to Access this file.')
